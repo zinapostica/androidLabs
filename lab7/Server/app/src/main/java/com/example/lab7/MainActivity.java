1c00bbca63370c7859607369633dc2359015c3f4
CataaZin <chertita.catalin@gmail.com>
2021-01-07 22:57:19 +0200

    added lab 7

--- lab7/Server/app/src/main/java/com/example/lab7/MainActivity.java
+++ lab7/Server/app/src/main/java/com/example/lab7/MainActivity.java
@@ -1,139 +1,143 @@
 package com.example.lab7;
-
+import android.annotation.SuppressLint;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import java.io.BufferedReader;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.net.UnknownHostException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 
-/**
- * @author
- */
-class ClientSocket extends Thread {
-    public static int clientNum;
-    private Socket clientSocket;
-    private BufferedReader keyboardReader;
-    private DataInputStream input;
-    private DataOutputStream output;
-    private int id;
-
-    public ClientSocket(Socket clientSocket) throws IOException {
-        this.clientSocket = clientSocket;
-        ClientSocket.clientNum++;
-        this.id = ClientSocket.clientNum;
-        this.keyboardReader = new BufferedReader(new InputStreamReader(System.in));
-        this.input = new DataInputStream(clientSocket.getInputStream());
-        this.output = new DataOutputStream(clientSocket.getOutputStream());
-    }
-
-    public void run() {
-        try {
-            this.output.writeUTF("Weclome to our booking system! Type in a seat number to check if it is have been reserved or not ");
-            boolean booked = false;
-            while (!booked && !this.clientSocket.isClosed()) {
-                String input = this.input.readUTF();
-                if (input.matches("[0-9]+")) {
-                    boolean busy = false;
-                    for (String position : Server.getPozitiiOcupate()) {
-                        if (position.equals(input)) {
-                            this.output.writeUTF(input + " has already been booked, please try another seat");
-                                    busy = true;
-                            break;
-                        }
-                    }
-                    if (Server.getPozitiiOcupate().size() == 0 || !busy) {
-                        this.output.writeUTF(input + " is available, would you like to book it? (y/n)");
-                        busy = false;
-                        do {
-                            String input2 = this.input.readUTF();
-                            switch (input2) {
-                                case "y":
-                                    Server.getPozitiiOcupate().add(input);
-                                    this.output.writeUTF("You successfully booked position number " +
-                                            input + ", thank you!");
-                                    booked = true;
-                                    busy = true;
-                                    break;
-                                case "n":
-                                    this.output.writeUTF("You chose not to book position number " + input
-                                            + ", please choose another position");
-                                    busy = true;
-                                    break;
-                                default:
-                                    this.output.writeUTF("Invalid response, please choose y or n");
-                            }
-                        } while (!busy);
-                    }
-                } else {
-                    this.output.writeUTF("Please enter a number, no letters");
-                }
-            }
-        } catch (IOException ex) {
-            Logger.getLogger(ClientSocket.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-}
-
-class Server extends Thread {
-    private final int port;
-    private static ArrayList<String> pozitiiOcupate;
-    private ServerSocket serverSocket;
-    private ArrayList<ClientSocket> clientSockets;
-
-    public Server(int port) {
-        this.port = port;
-        this.pozitiiOcupate = new ArrayList<String>();
-        this.pornesteServer();
-        this.clientSockets = new ArrayList<ClientSocket>();
-    }
-
-    public static ArrayList<String> getPozitiiOcupate() {
-        return pozitiiOcupate;
-    }
-
-    public boolean pornesteServer() {
-        try {
-            this.serverSocket = new ServerSocket(this.port);
-            System.out.println("Server: Server running at port " + this.port);
-            return true;
-        } catch (IOException ex) {
-            Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
-            System.out.println("Server: Error at starting the server");
-            return false;
-        }
-    }
-
-    public void run() {
-        while (!this.serverSocket.isClosed()) {
-            try {
-                this.clientSockets.add(new ClientSocket(serverSocket.accept()));
-                System.out.println("server: New Client Connection");
-                this.clientSockets.get(this.clientSockets.size() - 1).start();
-            } catch (IOException ex) {
-                Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
-                System.out.println("Server: Error connecting with client");
-            }
-        }
-    }
-}
-
-
+@SuppressLint("SetTextI18n")
 public class MainActivity extends AppCompatActivity {
-
+    ServerSocket serverSocket;
+    Thread Thread1 = null;
+    TextView tvIP, tvPort;
+    TextView tvMessages;
+    EditText etMessage;
+    Button btnSend;
+    public static String SERVER_IP = "";
+    public static final int SERVER_PORT = 8080;
+    String message;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Server server = new Server(9876);
-        server.start();
+        tvIP = findViewById(R.id.tvIP);
+        tvPort = findViewById(R.id.tvPort);
+        tvMessages = findViewById(R.id.tvMessages);
+        etMessage = findViewById(R.id.etMessage);
+        btnSend = findViewById(R.id.btnSend);
+        try {
+            SERVER_IP = getLocalIpAddress();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        Thread1 = new Thread(new Thread1());
+        Thread1.start();
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                message = etMessage.getText().toString().trim();
+                if (!message.isEmpty()) {
+                    new Thread(new Thread3(message)).start();
+                }
+            }
+        });
+    }
+    private String getLocalIpAddress() throws UnknownHostException {
+        WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
+        assert wifiManager!=null;
+        WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+        int ipInt = wifiInfo.getIpAddress();
+        return InetAddress.getByAddress(ByteBuffer.allocate(4).order(ByteOrder.LITTLE_ENDIAN).putInt(ipInt).array()).getHostAddress();
+    }
+    private PrintWriter output;
+    private BufferedReader input;
+    class Thread1 implements Runnable {
+        @Override
+        public void run() {
+            Socket socket;
+            try {
+                serverSocket = new ServerSocket(SERVER_PORT);
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        tvMessages.setText("Not connected");
+                        tvIP.setText("IP: " + SERVER_IP);
+                        tvPort.setText("Port: " + String.valueOf(SERVER_PORT));
+                    }
+                });
+                try {
+                    socket = serverSocket.accept();
+                    output = new PrintWriter(socket.getOutputStream());
+                    input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            tvMessages.setText("Connected\n");
+                        }
+                    });
+                    new Thread(new Thread2()).start();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    private class Thread2 implements Runnable {
+        @Override
+        public void run() {
+            while (true) {
+                try {
+                    final String message = input.readLine();
+                    if (message!=null) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                tvMessages.append("client:" + message + "\n");
+                            }
+                        });
+                    } else {
+                        Thread1 = new Thread(new Thread1());
+                        Thread1.start();
+                        return;
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+    class Thread3 implements Runnable {
+        private String message;
+        Thread3(String message) {
+            this.message = message;
+        }
+        @Override
+        public void run() {
+            output.write(message);
+            output.flush();
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    tvMessages.append("server: " + message + "\n");
+                    etMessage.setText("");
+                }
+            });
+        }
     }
 }
